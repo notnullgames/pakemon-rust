b5523b07b216c95fa4690f012fa23014b85b0913
David Konsumer <konsumer@jetboystudio.com>
2021-04-05 01:57:19 -0700

    fleshing out types for rattata lib

---
+++ src/rattata_client/lib.rs
@@ -0,0 +1,24 @@
+pub struct Minion {
+    id: String,
+    platform: String,
+    arch: String,
+    plugins: Vec<String>,
+    ram: i32,  // in GB
+    disk: i32, // in GB
+    has_root: bool,
+}
+
+impl Minion {
+    // run a command on this minion
+    pub fn command(_command: String, _arguments: Vec<String>) {}
+
+    // install a plugin on this minion
+    pub fn install(_plugin: String) {}
+}
+
+pub struct Rattata {
+    // list of connected minions
+    pub minions: Vec<Minion>,
+}
+
+impl Rattata {}
