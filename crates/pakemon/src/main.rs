4104863ef4e4eb50e65a896b10c9049496395db9
David Konsumer <konsumer@jetboystudio.com>
2021-04-08 22:09:41 -0700

    no bevy_retro for now, due to licensing

--- crates/pakemon/src/main.rs
+++ crates/pakemon/src/main.rs
@@ -1,9 +1,4 @@
 use bevy::prelude::*;
-use bevy_retro::*;
-
-// Create a stage label that will be used for our game logic stage
-#[derive(StageLabel, Debug, Eq, Hash, PartialEq, Clone)]
-struct GameStage;
 
 fn main() {
     App::build()
@@ -13,33 +8,19 @@ fn main() {
             height: 240.0,
             ..Default::default()
         })
-        .add_plugins(RetroPlugins)
+        .add_plugins(DefaultPlugins)
         .add_startup_system(setup.system())
         .run();
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>, _scene_graph: ResMut<SceneGraph>) {
-    // Load our sprites
-    let title_image = asset_server.load("title.png");
-
-    // Spawn the camera
-    commands.spawn().insert_bundle(CameraBundle {
-        camera: Camera {
-            // Set our camera to have a fixed height and an auto-resized width
-            size: CameraSize::FixedHeight(240),
-            background_color: Color::new(0.0, 0.0, 0.0, 1.0),
-            ..Default::default()
-        },
-        ..Default::default()
-    });
-
-    // Spawn the title-logo
-    commands.spawn().insert_bundle(SpriteBundle {
-        image: title_image,
-        position: Position::new(0, 0, -1),
-        sprite: Sprite {
-            ..Default::default()
-        },
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(SpriteBundle {
+        material: materials.add(asset_server.load("title.png").into()),
         ..Default::default()
     });
 }
